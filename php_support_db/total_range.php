534b5bd4f5bbbd887e21cffb5ccaf38941413d87
Dg <ash3767@naver.com>
2019-10-13 08:10:33 +0900

    total_range_modify

--- php_support_db/total_range.php
+++ php_support_db/total_range.php
@@ -41,21 +41,21 @@ if(isset($_POST["date"])){
         ';
 
         if($today_data_list[0] > 0){
-            $result.='<button id="move_sk7" class="btn btn-outline-danger button_size">SKT </br>'.$today_data_list[0].'건</button>';
+            $result.='<button id="move_skt_world" class="btn btn-outline-danger button_size">SKT </br>'.$today_data_list[0].'건</button>';
         }else {
-            $result.='<button id="move_sk7" class="btn btn-outline-info button_size">SKT </br>'.$today_data_list[0].'건</button>';
+            $result.='<button id="move_skt_world" class="btn btn-outline-info button_size">SKT </br>'.$today_data_list[0].'건</button>';
         }
 
         if($today_data_list[1] > 0){
-            $result.='<button id="move_sk7" class="btn btn-outline-danger button_size">KT </br>'.$today_data_list[1].'건</button>';
+            $result.='<button id="move_kt_shop" class="btn btn-outline-danger button_size">KT </br>'.$today_data_list[1].'건</button>';
         }else {
-            $result.='<button id="move_sk7" class="btn btn-outline-info button_size">KT </br>'.$today_data_list[1].'건</button>';
+            $result.='<button id="move_kt_shop" class="btn btn-outline-info button_size">KT </br>'.$today_data_list[1].'건</button>';
         }
 
         if($today_data_list[2] > 0){
-            $result.='<button id="move_sk7" class="btn btn-outline-danger button_size">LG U+ </br>'.$today_data_list[2].'건</button>';
+            $result.='<button id="move_lg_u_plus" class="btn btn-outline-danger button_size">LG U+ </br>'.$today_data_list[2].'건</button>';
         }else {
-            $result.='<button id="move_sk7" class="btn btn-outline-info button_size">LG U+ </br>'.$today_data_list[2].'건</button>';
+            $result.='<button id="move_lg_u_plus" class="btn btn-outline-info button_size">LG U+ </br>'.$today_data_list[2].'건</button>';
         }
         $result.='   
         </div>';
@@ -64,9 +64,9 @@ if(isset($_POST["date"])){
             ';
 
             if($today_data_list[3] > 0){
-                $result.='<button id="move_sk7" class="btn btn-outline-danger button_size">U+알뜰모바일 </br>'.$today_data_list[3].'건</button>';
+                $result.='<button id="move_lg_mobile" class="btn btn-outline-danger button_size">U+알뜰모바일 </br>'.$today_data_list[3].'건</button>';
             }else {
-                $result.='<button id="move_sk7" class="btn btn-outline-info button_size">U+알뜰모바일 </br>'.$today_data_list[3].'건</button>';
+                $result.='<button id="move_lg_mobile" class="btn btn-outline-info button_size">U+알뜰모바일 </br>'.$today_data_list[3].'건</button>';
             }
 
             if($today_data_list[4] > 0){
@@ -76,9 +76,9 @@ if(isset($_POST["date"])){
             }
 
             if($today_data_list[5] > 0){
-                $result.='<button id="move_sk7" class="btn btn-outline-danger button_size">KT M모바일 </br>'.$today_data_list[5].'건</button>';
+                $result.='<button id="move_kt_m_mobile" class="btn btn-outline-danger button_size">KT M모바일 </br>'.$today_data_list[5].'건</button>';
             }else {
-                $result.='<button id="move_sk7" class="btn btn-outline-info button_size">KT M모바일 </br>'.$today_data_list[5].'건</button>';
+                $result.='<button id="move_kt_m_mobile" class="btn btn-outline-info button_size">KT M모바일 </br>'.$today_data_list[5].'건</button>';
             }
 
             if($today_data_list[6] > 0){
